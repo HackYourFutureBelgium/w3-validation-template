610325363da89d7d41802907433e66a491a69810
colevandersWands <18554853+colevandersWands@users.noreply.github.com>
2020-10-11 15:21:32 +0200

    accessibility check

--- evaluation/init.js
+++ evaluation/init.js
@@ -4,25 +4,26 @@ if (window.location.href.indexOf('http') !== -1) {
     validateContainer.style = 'text-align:center;';
 
     const validateHTML = document.createElement('button');
-    // how would you label this button with ARIA?
     validateHTML.innerHTML = 'validate HTML';
     validateHTML.onclick = () => window.open('https://validator.w3.org/nu/?doc=' + encodeURIComponent(window.location.href), '_blank');
     validateContainer.appendChild(validateHTML);
 
     const validateCSS = document.createElement('button');
-    // how would you label this button with ARIA?
     validateCSS.innerHTML = 'validate CSS';
     validateCSS.onclick = () => window.open('https://jigsaw.w3.org/css-validator/validator?uri=' + encodeURIComponent(window.location.href), '_blank');
     validateContainer.appendChild(validateCSS);
 
+    const accessibilityCheckButton = document.createElement('button');
+    accessibilityCheckButton.innerHTML = 'accessibility check';
+    accessibilityCheckButton.onclick = () => window.open(`https://wave.webaim.org/report#/${encodeURIComponent(window.location.href)}`, '_blank');
+    validateContainer.appendChild(accessibilityCheckButton);
+
     const spellCheckButton = document.createElement('button');
-    // how would you label this button with ARIA?
     spellCheckButton.innerHTML = 'spelling check';
     spellCheckButton.onclick = () => window.open(`https://datayze.com/website-spell-checker?domain=${encodeURIComponent(window.location.href)}&parameters=&exclude=&elementtype=limit&elements=&ignored=`, '_blank');
     validateContainer.appendChild(spellCheckButton);
 
     const developmentStrategyButton = document.createElement('button');
-    // how would you label this button with ARIA?
     developmentStrategyButton.innerHTML = 'development strategy';
     const developmentStrategyA = document.createElement('a');
     developmentStrategyA.href = './development-strategy';
